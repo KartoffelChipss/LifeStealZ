86911ae81f9c5f5767ada1daf96ac391baad3b58
KartoffelChips <jan@strassburger.org>
2024-04-26 18:47:32 +0200

    Fixed memory leaks caused by the new Storage system

--- src/main/java/org/strassburger/lifestealz/util/storage/SQLitePlayerDataStorage.java
+++ src/main/java/org/strassburger/lifestealz/util/storage/SQLitePlayerDataStorage.java
@@ -12,8 +12,11 @@ public class SQLitePlayerDataStorage implements PlayerDataStorage {
     public void init() {
         try (Connection connection = createConnection()) {
             if (connection == null) return;
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS hearts (uuid TEXT PRIMARY KEY, name TEXT, maxhp REAL, hasbeenRevived INTEGER, craftedHearts INTEGER, craftedRevives INTEGER, killedOtherPlayers INTEGER)");
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS hearts (uuid TEXT PRIMARY KEY, name TEXT, maxhp REAL, hasbeenRevived INTEGER, craftedHearts INTEGER, craftedRevives INTEGER, killedOtherPlayers INTEGER)");
+            } catch (SQLException e) {
+                LifeStealZ.getInstance().getLogger().severe("Failed to initialize SQLite database: " + e.getMessage());
+            }
         } catch (SQLException e) {
             LifeStealZ.getInstance().getLogger().severe("Failed to initialize SQLite database: " + e.getMessage());
         }
@@ -34,8 +37,11 @@ public class SQLitePlayerDataStorage implements PlayerDataStorage {
     public void save(PlayerData playerData) {
         try (Connection connection = createConnection()) {
             if (connection == null) return;
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("INSERT OR REPLACE INTO hearts (uuid, name, maxhp, hasbeenRevived, craftedHearts, craftedRevives, killedOtherPlayers) VALUES ('" + playerData.getUuid() + "', '" + playerData.getName() + "', " + playerData.getMaxhp() + ", " + playerData.getHasbeenRevived() + ", " + playerData.getCraftedHearts() + ", " + playerData.getCraftedRevives() + ", " + playerData.getKilledOtherPlayers() + ")");
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate("INSERT OR REPLACE INTO hearts (uuid, name, maxhp, hasbeenRevived, craftedHearts, craftedRevives, killedOtherPlayers) VALUES ('" + playerData.getUuid() + "', '" + playerData.getName() + "', " + playerData.getMaxhp() + ", " + playerData.getHasbeenRevived() + ", " + playerData.getCraftedHearts() + ", " + playerData.getCraftedRevives() + ", " + playerData.getKilledOtherPlayers() + ")");
+            } catch (SQLException e) {
+                LifeStealZ.getInstance().getLogger().severe("Failed to save player data to SQLite database: " + e.getMessage());
+            }
         } catch (SQLException e) {
             LifeStealZ.getInstance().getLogger().severe("Failed to save player data to SQLite database: " + e.getMessage());
         }
@@ -45,17 +51,25 @@ public class SQLitePlayerDataStorage implements PlayerDataStorage {
     public PlayerData load(UUID uuid) {
         try (Connection connection = createConnection()) {
             if (connection == null) return null;
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM hearts WHERE uuid = '" + uuid + "'");
-            if (!resultSet.next()) return null;
-            PlayerData playerData = new PlayerData(resultSet.getString("name"), uuid);
-            playerData.setMaxhp(resultSet.getDouble("maxhp"));
-            playerData.setHasbeenRevived(resultSet.getInt("hasbeenRevived"));
-            playerData.setCraftedHearts(resultSet.getInt("craftedHearts"));
-            playerData.setCraftedRevives(resultSet.getInt("craftedRevives"));
-            playerData.setKilledOtherPlayers(resultSet.getInt("killedOtherPlayers"));
-            return playerData;
+            try (Statement statement = connection.createStatement()) {
+                statement.setQueryTimeout(30);
+                try (ResultSet resultSet = statement.executeQuery("SELECT * FROM hearts WHERE uuid = '" + uuid + "'");) {
+                    if (!resultSet.next()) return null;
+                    PlayerData playerData = new PlayerData(resultSet.getString("name"), uuid);
+                    playerData.setMaxhp(resultSet.getDouble("maxhp"));
+                    playerData.setHasbeenRevived(resultSet.getInt("hasbeenRevived"));
+                    playerData.setCraftedHearts(resultSet.getInt("craftedHearts"));
+                    playerData.setCraftedRevives(resultSet.getInt("craftedRevives"));
+                    playerData.setKilledOtherPlayers(resultSet.getInt("killedOtherPlayers"));
+                    return playerData;
+                } catch (SQLException e) {
+                    LifeStealZ.getInstance().getLogger().severe("Failed to load player data from SQLite database: " + e.getMessage());
+                    return null;
+                }
+            } catch (SQLException e) {
+                LifeStealZ.getInstance().getLogger().severe("Failed to load player data from SQLite database: " + e.getMessage());
+                return null;
+            }
         } catch (SQLException e) {
             LifeStealZ.getInstance().getLogger().severe("Failed to load player data from SQLite database: " + e.getMessage());
             return null;
@@ -74,13 +88,16 @@ public class SQLitePlayerDataStorage implements PlayerDataStorage {
         try (Connection connection = createConnection()) {
             if (connection == null) return eliminatedPlayers;
 
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
+            try (Statement statement = connection.createStatement()) {
+                statement.setQueryTimeout(30);
 
-            ResultSet resultSet = statement.executeQuery("SELECT uuid FROM hearts WHERE maxhp <= 0.0");
+                ResultSet resultSet = statement.executeQuery("SELECT uuid FROM hearts WHERE maxhp <= 0.0");
 
-            while (resultSet.next()) {
-                eliminatedPlayers.add(UUID.fromString(resultSet.getString("uuid")));
+                while (resultSet.next()) {
+                    eliminatedPlayers.add(UUID.fromString(resultSet.getString("uuid")));
+                }
+            } catch (SQLException e) {
+                LifeStealZ.getInstance().getLogger().severe("Failed to load player data from SQLite database: " + e.getMessage());
             }
         } catch (SQLException e) {
             LifeStealZ.getInstance().getLogger().severe("Failed to load player data from SQLite database: " + e.getMessage());
