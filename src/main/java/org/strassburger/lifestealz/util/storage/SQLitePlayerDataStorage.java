bd9de9df4ee73482b43b13e79b83424b1f6701f9
KartoffelChipss <jan@strassburger.org>
2024-05-25 18:07:48 +0200

    now creating new playerData entry if none existing

--- src/main/java/org/strassburger/lifestealz/util/storage/SQLitePlayerDataStorage.java
+++ src/main/java/org/strassburger/lifestealz/util/storage/SQLitePlayerDataStorage.java
@@ -1,5 +1,7 @@
 package org.strassburger.lifestealz.util.storage;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.strassburger.lifestealz.LifeStealZ;
 
 import java.sql.*;
@@ -54,13 +56,22 @@ public class SQLitePlayerDataStorage implements PlayerDataStorage {
             try (Statement statement = connection.createStatement()) {
                 statement.setQueryTimeout(30);
                 try (ResultSet resultSet = statement.executeQuery("SELECT * FROM hearts WHERE uuid = '" + uuid + "'");) {
-                    if (!resultSet.next()) return null;
+
+                    if (!resultSet.next()) {
+                        Player player = Bukkit.getPlayer(uuid);
+                        if (player == null) return null;
+                        PlayerData newPlayerData = new PlayerData(player.getName(), uuid);
+                        save(newPlayerData);
+                        return newPlayerData;
+                    }
+
                     PlayerData playerData = new PlayerData(resultSet.getString("name"), uuid);
                     playerData.setMaxhp(resultSet.getDouble("maxhp"));
                     playerData.setHasbeenRevived(resultSet.getInt("hasbeenRevived"));
                     playerData.setCraftedHearts(resultSet.getInt("craftedHearts"));
                     playerData.setCraftedRevives(resultSet.getInt("craftedRevives"));
                     playerData.setKilledOtherPlayers(resultSet.getInt("killedOtherPlayers"));
+
                     return playerData;
                 } catch (SQLException e) {
                     LifeStealZ.getInstance().getLogger().severe("Failed to load player data from SQLite database: " + e.getMessage());
