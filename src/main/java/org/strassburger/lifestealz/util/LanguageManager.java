d4689211c0e30022fc2134b61126f4e3216d7d27
KartoffelChips <jan@strassburger.org>
2024-09-09 16:28:57 +0200

    Updated LanguageManager

--- src/main/java/org/strassburger/lifestealz/util/LanguageManager.java
+++ src/main/java/org/strassburger/lifestealz/util/LanguageManager.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class LanguageManager {
     private final JavaPlugin plugin;
-    public static final List<String> defaultLangs = List.of("cs-CZ", "de-DE", "en-US", "es-ES", "fr-FR", "pl-PL", "vi-VN", "zh-CN");
+    public static final List<String> defaultLangs = List.of("cs-CZ", "de-DE", "en-US", "es-ES", "fr-FR", "pl-PL", "vi-VN", "zh-CN", "nl-NL");
 
     private FileConfiguration langConfig;
 
