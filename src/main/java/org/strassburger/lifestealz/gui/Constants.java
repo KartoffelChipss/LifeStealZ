f8e58a8f59b19cd4d9e83839a61586dbbe9a6e28
Abb3v <166838215+abb3v@users.noreply.github.com>
2024-11-27 16:16:46 +1100

    Abstract-ification [Temporary Solution]

--- src/main/java/org/strassburger/lifestealz/gui/Constants.java
+++ src/main/java/org/strassburger/lifestealz/gui/Constants.java
@@ -1,58 +1,91 @@
 package org.strassburger.lifestealz.gui;
 
-// org.strassburger.lifestealz.gui developed by abb3v
-
 import net.kyori.adventure.text.format.TextColor;
 
 public final class Constants {
-    // Prevent instantiation
     private Constants() {}
 
-    public static final class Colors {
-        public static final TextColor TITLE = TextColor.color(0xFF6B6B);
-        public static final TextColor SUPPORT = TextColor.color(0xFED766);
-        public static final TextColor DISCORD = TextColor.color(0x7289DA);
-        public static final TextColor WIKI = TextColor.color(0x4ECB71);
-        public static final TextColor REVIEW = TextColor.color(0xFFB347);
-        public static final TextColor DESCRIPTION = TextColor.color(0xE0E0E0);
-        public static final TextColor HOVER = TextColor.color(0x72CEE3);
-        public static final TextColor LOVE = TextColor.color(0xFF7F7E);
-        public static final TextColor GREY = TextColor.color(0x555555);
-        public static final TextColor BACK = TextColor.color(0xFF4444);
-        public static final TextColor DISABLED = TextColor.color(0x808080);
+    public enum Colors {
+        TITLE(0xFF6B6B),
+        SUPPORT(0xFED766),
+        DISCORD(0x7289DA),
+        WIKI(0x4ECB71),
+        REVIEW(0xFFB347),
+        DESCRIPTION(0xE0E0E0),
+        HOVER(0x72CEE3),
+        LOVE(0xFF7F7E),
+        GREY(0x555555),
+        BACK(0xFF4444),
+        DISABLED(0x808080);
+
+        private final TextColor value;
+
+        Colors(int hexColor) {
+            this.value = TextColor.color(hexColor);
+        }
+
+        public TextColor getValue() {
+            return value;
+        }
     }
 
-    public static final class Icons {
-        public static final String GUI_ICON = "☠";
-        public static final String SEPARATOR = " | ";
-        public static final String ARROW_LEFT = "«";
-        public static final String ARROW_RIGHT = "»";
+    public enum Icons {
+        GUI_ICON("☠"),
+        SEPARATOR(" | "),
+        ARROW_LEFT("«"),
+        ARROW_RIGHT("»");
+
+        private final String icon;
+
+        Icons(String icon) {
+            this.icon = icon;
+        }
+
+        public String getValue() {
+            return icon;
+        }
     }
 
-    public static final class Urls {
-        public static final String DISCORD = "https://strassburger.org/discord/";
-        public static final String WIKI = "https://lsz.strassburger.dev/";
-        public static final String REVIEW = "https://www.spigotmc.org/resources/lifestealz.111469/";
+    public enum Textures {
+        SUPPORT("http://textures.minecraft.net/texture/d34e063cafb467a5c8de43ec78619399f369f4a52434da8017a983cdd92516a0"),
+        DISCORD("http://textures.minecraft.net/texture/92ca27cab87722896dc668c7b9b76e6fc3e2dce770583adfbe162adc94e9d82d"),
+        WIKI("http://textures.minecraft.net/texture/18c0ca8fae701314330fdfdded3445e4542a3597b70d38f47d75111eb04104b8"),
+        REVIEW("http://textures.minecraft.net/texture/e34a592a79397a8df3997c43091694fc2fb76c883a76cce89f0227e5c9f1dfe"),
+        DEVELOPER("http://textures.minecraft.net/texture/fa2d6b3f7a49a62dcf8091f9077b68f53c4cbb136e6b96d5266f3d8a152a5165"),
+        GRAVEYARD("http://textures.minecraft.net/texture/a082e31b2d7b3a3ec3c451591a6feb97d048f5392376e4e06a3fee7982571ca4"),
+        GHOST("http://textures.minecraft.net/texture/e0cfbdf63bb1bf8c989140a5fc54831d84cb0f8f64b4e1df7000db041258f5a1"),
+        DENY("http://textures.minecraft.net/texture/d08ee6edfa98db5eae9b9c9936e94489b2d4bbbd3d2b4b6b4885a32240613c"),
+        ACCEPT("http://textures.minecraft.net/texture/9b5871c72987266e15f1be49b1ec334ef6b618e9653fb78e918abd39563dbb93"),
+        ARROW_LEFT("http://textures.minecraft.net/texture/3ebf907494a935e955bfcadab81beafb90fb9be49c7026ba97d798d5f1a23"),
+        ARROW_RIGHT("http://textures.minecraft.net/texture/1b6f1a25b6bc199946472aedb370522584ff6f4e83221e5946bd2e41b5ca13b"),
+        HOME("http://textures.minecraft.net/texture/63d02cdc075bb1cc5f6fe3c7711ae4977e38b910d50ed6023df73913e5e7fcff"),
+        ARROW_LEFT_DISABLED("http://textures.minecraft.net/texture/bb0f6e8af46ac6faf88914191ab66f261d6726a7999c637cf2e4159fe1fc477"),
+        ARROW_RIGHT_DISABLED("http://textures.minecraft.net/texture/f2f3a2dfce0c3dab7ee10db385e5229f1a39534a8ba2646178e37c4fa93b");
+
+        private final String url;
+
+        Textures(String url) {
+            this.url = url;
+        }
+
+        public String getValue() {
+            return url;
+        }
     }
 
-    public static final class Textures {
-        public static final String SUPPORT = "http://textures.minecraft.net/texture/d34e063cafb467a5c8de43ec78619399f369f4a52434da8017a983cdd92516a0";
-        public static final String DISCORD = "http://textures.minecraft.net/texture/92ca27cab87722896dc668c7b9b76e6fc3e2dce770583adfbe162adc94e9d82d";
-        public static final String WIKI = "http://textures.minecraft.net/texture/18c0ca8fae701314330fdfdded3445e4542a3597b70d38f47d75111eb04104b8";
-        public static final String REVIEW = "http://textures.minecraft.net/texture/e34a592a79397a8df3997c43091694fc2fb76c883a76cce89f0227e5c9f1dfe";
-        public static final String DEVELOPER = "http://textures.minecraft.net/texture/fa2d6b3f7a49a62dcf8091f9077b68f53c4cbb136e6b96d5266f3d8a152a5165";
-        public static final String GRAVEYARD = "http://textures.minecraft.net/texture/a082e31b2d7b3a3ec3c451591a6feb97d048f5392376e4e06a3fee7982571ca4";
-        public static final String GHOST = "http://textures.minecraft.net/texture/e0cfbdf63bb1bf8c989140a5fc54831d84cb0f8f64b4e1df7000db041258f5a1";
+    public enum Urls {
+        DISCORD("https://strassburger.org/discord/"),
+        WIKI("https://lsz.strassburger.dev/"),
+        REVIEW("https://www.spigotmc.org/resources/lifestealz.111469/");
 
-        public static final String DENY = "http://textures.minecraft.net/texture/d08ee6edfa98db5eae9b9c9936e94489b2d4bbbd3d2b4b6b4885a32240613c";
-        public static final String ACCEPT = "http://textures.minecraft.net/texture/9b5871c72987266e15f1be49b1ec334ef6b618e9653fb78e918abd39563dbb93";
+        private final String url;
 
-        public static final String ARROW_LEFT = "http://textures.minecraft.net/texture/3ebf907494a935e955bfcadab81beafb90fb9be49c7026ba97d798d5f1a23";
-        public static final String ARROW_RIGHT = "http://textures.minecraft.net/texture/1b6f1a25b6bc199946472aedb370522584ff6f4e83221e5946bd2e41b5ca13b";
-        public static final String HOME = "http://textures.minecraft.net/texture/63d02cdc075bb1cc5f6fe3c7711ae4977e38b910d50ed6023df73913e5e7fcff";
-        public static final String ARROW_LEFT_DISABLED = "http://textures.minecraft.net/texture/bb0f6e8af46ac6faf88914191ab66f261d6726a7999c637cf2e4159fe1fc477";
-        public static final String ARROW_RIGHT_DISABLED = "http://textures.minecraft.net/texture/f2f3a2dfce0c3dab7ee10db385e5229f1a39534a8ba2646178e37c4fa93b";
+        Urls(String url) {
+            this.url = url;
+        }
 
-
+        public String getValue() {
+            return url;
+        }
     }
 }
